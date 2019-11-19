b0761f2fa7e9ae7637a313cf5fca406d9e08688f
Nisho Kumaran <nishokumaran4@outlook.com>
2019-11-19 13:15:21 +0530

    oild

--- azureautobuildsl/azureautobuild/Controllers/ValuesController.cs
+++ azureautobuildsl/azureautobuild/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace azureautobuild.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value" };
+            return new string[] { "value1", "valuegnghnhgn" };
         }
 
         // GET api/values/5
